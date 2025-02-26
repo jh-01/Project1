54ab80997eab5f6c0d389f9969735eab548a1d39
jh-01 <were1117@sm.ac.kr>
2025-02-26 15:38:00 +0900

    Initial commit

---
+++ js/default.js
@@ -0,0 +1,8 @@
+// 예시: 사용되지 않는 변수 제거
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+// 예시: 사용되지 않는 변수 제거
+// const onRequest = ...;
+// const logger = ...;
